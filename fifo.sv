30f26d4824b6bbf35b572bd69d9f01ecb50904be
Ritik-kumar430 <ritikkumar0430@gmail.com>
2025-09-11 14:40:05 +0530

    Create fifo.sv

---
+++ fifo.sv
@@ -0,0 +1,14 @@
+interface dff_if(input logic clk);
+  logic rst;
+  logic din;
+  logic dout;
+endinterface
+
+module dff(dff_if vif);
+  always_ff @(posedge vif.clk or posedge vif.rst) begin
+    if (vif.rst)
+      vif.dout <= 0;
+    else
+      vif.dout <= vif.din;
+  end
+endmodule  
